d2d25b12cb4e24150c45196471eeb7533d6ea77a
Samyak Padwekar <samyaksam007@gmail.com>
2020-10-19 17:56:36 +0530

    Signup Class modified

--- ATM Simulator System/src/atm/simulator/system/Signup.java
+++ ATM Simulator System/src/atm/simulator/system/Signup.java
@@ -141,7 +141,7 @@ public class Signup extends JFrame implements ActionListener{
         
         
      
-        String date[] = {"1","2","3","4","5","6","7","8","9"};
+        String date[] = {"1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30"};
         c1 = new JComboBox(date);
         c1.setBackground(Color.WHITE);
         
